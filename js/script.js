e29f25ba3e4b3f4072bf06d7e67638af79eecc9f
Marn39 <210750650+Marn39@users.noreply.github.com>
2026-01-23 20:27:43 +0000

    Created a readme file to discuss the purpose, application, and user interaction.

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@
 document.getElementById("buttonstart").addEventListener("click", startBudget);
 
 
-
+// If you press the button before entering in a number, a message will appear instructing you to. 
 function startBudget() {
 var budget = document.getElementById("holidaybudget").value;
 if (budget === ""){
@@ -15,12 +15,14 @@ return;
  var prices = document.getElementsByClassName("Price");
  var totalprices = 0;
 
+ // These functions will calculate the total for the expenses outlined in the table.
 for (var i = 0; i < prices.length; i++){
  if (prices[i].value !== "") {
   totalprices = totalprices + Number(prices[i].value);
 }
   }
 
+  // That total will then be substracted from the pre-holiday budget. 
   var remaining = budget - totalprices;
   document.getElementById("prebudget").innerHTML = 
   "Here is your remaining budget of £" + remaining;
