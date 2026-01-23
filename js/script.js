9e25be733c8e0121b4011a51cedd58b7f618e678
Marn39 <210750650+Marn39@users.noreply.github.com>
2026-01-23 15:45:13 +0000

    created JS to ensure when the user enters in a number, then clicks confirm budget, the text will showcase what that budget is.

--- js/script.js
+++ js/script.js
@@ -2,5 +2,8 @@ document.getElementById("buttonstart").addEventListener("click", startBudget);
 
 function startBudget() {
   document.getElementById("letsgetstarted").innerHTML = "Now you're ready, let's begin adding some expenses!";
+  var budget = document.getElementById("holidaybudget").value;
+  document.getElementById("prebudget").innerHTML = 
+  "Here is your pre-holiday budget £" + budget;
 }
 
