25b767054f69351c1a185bdf0beaf7e01a3164aa
Marn39 <210750650+Marn39@users.noreply.github.com>
2026-01-23 16:01:48 +0000

    added an if statement for the buttons on my application.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,16 @@
 document.getElementById("buttonstart").addEventListener("click", startBudget);
 
 function startBudget() {
+var budget = document.getElementById("holidaybudget").value; 
+
+if (budget === ""){
+  document.getElementById("prebudget").innerHTML = "You must enter your holidaay budget first";
+return;
+
+}
+
   document.getElementById("letsgetstarted").innerHTML = "Now you're ready, let's begin adding some expenses!";
-  var budget = document.getElementById("holidaybudget").value;
   document.getElementById("prebudget").innerHTML = 
   "Here is your pre-holiday budget £" + budget;
-}
+  }
 
