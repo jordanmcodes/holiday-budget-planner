6d843a984dcee1a9e0dabefa1a5b28db9b125d69
Marn39 <210750650+Marn39@users.noreply.github.com>
2026-01-23 10:12:20 +0000

    Added an extra paragraph and function to the javascript to show what the remaining holiday budget will be.

--- js/script.js
+++ js/script.js
@@ -3,4 +3,6 @@ document.getElementById("buttonstart").addEventListener("click", startBudget);
 function startBudget() {
   var budget = document.getElementById("holidaybudget").value;
   document.getElementById("letsgetstarted").innerHTML = "Your holiday budget is £" + budget + ". Now you're ready, let's begin adding some expenses!";
+
+ document.getElementById("budgetleft").innerHTML = "Your remaining holiday budget is" + budget;
 }
