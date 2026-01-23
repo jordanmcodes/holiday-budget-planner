772f8f7e2c6f5b36be4f7c210b3f8434c3d218dc
Marn39 <210750650+Marn39@users.noreply.github.com>
2026-01-23 19:09:49 +0000

    Attempt to fix the JSlint issues.

--- js/script.js
+++ js/script.js
@@ -1,23 +1,23 @@
 document.getElementById("buttonstart").addEventListener("click", startBudget);
 
 function startBudget() {
-var budget = document.getElementById("holidaybudget").value; 
-
+var budget = document.getElementById("holidaybudget").value;
 if (budget === ""){
-  document.getElementById("prebudget").innerHTML = "You must enter your holiday budget first";
+  document.getElementById("prebudget").innerHTML = 
+  "You must enter your holiday budget first";
 return;
 }
  else {
  var prices = document.getElementsByClassName("Price");
  var totalprices = 0;
 
-for (let i = 0; i < prices.length; i++){
+for (var i = 0; i < prices.length; i++){
  if (prices[i].value !== "") {
   totalprices = totalprices + Number(prices[i].value);
 }
   }
 
-  var remaining = budget - totalprices
+  var remaining = budget - totalprices;
   document.getElementById("prebudget").innerHTML = 
   "Here is your remaining budget of £" + remaining;
 }
