7ea82dd2fbe5ffcd66b31b12d581534bd1074aa8
Marn39 <210750650+Marn39@users.noreply.github.com>
2026-01-23 13:29:16 +0000

    reverted js back to original format where the button simply showcases a paragraph when clicked.

--- js/script.js
+++ js/script.js
@@ -1,8 +1,5 @@
 document.getElementById("buttonstart").addEventListener("click", startBudget);
 
 function startBudget() {
-  var budget = document.getElementById("holidaybudget").value;
-  document.getElementById("letsgetstarted").innerHTML = "Your holiday budget is £" + budget + ". Now you're ready, let's begin adding some expenses!";
-
- document.getElementById("budgetleft").innerHTML = "Your remaining holiday budget is £" + budget;
+  document.getElementById("letsgetstarted").innerHTML = "Now you're ready, let's begin adding some expenses!";
 }
