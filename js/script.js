09c8e4592a0b714a3a9b221314856af2e7d6799b
Marn39 <210750650+Marn39@users.noreply.github.com>
2026-01-23 13:30:54 +0000

    moved remaining budget paragraph to below the table.

--- js/script.js
+++ js/script.js
@@ -3,3 +3,4 @@ document.getElementById("buttonstart").addEventListener("click", startBudget);
 function startBudget() {
   document.getElementById("letsgetstarted").innerHTML = "Now you're ready, let's begin adding some expenses!";
 }
+
