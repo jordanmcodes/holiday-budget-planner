7adb04c2562483c7dc77dc01c7c0840925e35a26
Marn39 <210750650+Marn39@users.noreply.github.com>
2026-02-08 17:31:58 +0000

    Formatted the document so that the appropriate code is indented.

--- js/script.js
+++ js/script.js
@@ -5,26 +5,26 @@ document.getElementById("buttonstart").addEventListener("click", startBudget);
 
 // If you press the button before entering in a number, a message will appear instructing you to. 
 function startBudget() {
-var budget = document.getElementById("holidaybudget").value;
-if (budget === ""){
-  document.getElementById("prebudget").innerHTML = 
-  "You must enter your holiday budget first";
-return;
-}
- else {
- var prices = document.getElementsByClassName("Price");
- var totalprices = 0;
-
- // These functions will calculate the total for the expenses outlined in the table.
-for (var i = 0; i < prices.length; i++){
- if (prices[i].value !== "") {
-  totalprices = totalprices + Number(prices[i].value);
-}
+  var budget = document.getElementById("holidaybudget").value;
+  if (budget === "") {
+    document.getElementById("prebudget").innerHTML =
+      "You must enter your holiday budget first";
+    return;
   }
+  else {
+    var prices = document.getElementsByClassName("Price");
+    var totalprices = 0;
 
-  // That total will then be substracted from the pre-holiday budget. 
-  var remaining = budget - totalprices;
-  document.getElementById("prebudget").innerHTML = 
-  "Here is your remaining budget of £" + remaining;
-}
+    // These functions will calculate the total for the expenses outlined in the table.
+    for (var i = 0; i < prices.length; i++) {
+      if (prices[i].value !== "") {
+        totalprices = totalprices + Number(prices[i].value);
+      }
+    }
+
+    // That total will then be substracted from the pre-holiday budget. 
+    var remaining = budget - totalprices;
+    document.getElementById("prebudget").innerHTML =
+      "Here is your remaining budget of £" + remaining;
+  }
 }
