0045a47e0ed024726b3b345499148948d03e17a0
Marn39 <210750650+Marn39@users.noreply.github.com>
2026-02-09 20:20:18 +0000

    attempted changes to fix js lint

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,32 @@
 
-// When the button is pressed, the process begins. 
+// When the button is pressed, the process begins.
 document.getElementById("buttonstart").addEventListener("click", startBudget);
 
 
-// If you press the button before entering in a number, a message will appear instructing you to. 
+// If you press the button before entering in a number,
+// A message will appear instructing you to.
 function startBudget() {
+
   var budget = document.getElementById("holidaybudget").value;
   if (budget === "") {
     document.getElementById("prebudget").innerHTML =
       "You must enter your holiday budget first";
     return;
+  
   }
   else {
-    var prices = document.getElementsByClassName("Price");
-    var totalprices = 0;
+     var prices = document.getElementsByClassName("Price");
+     var totalprices = 0;
 
-    // These functions will calculate the total for the expenses outlined in the table.
-    for (var i = 0; i < prices.length; i++) {
+    // These functions will calculate the total
+    // for the expenses outlined in the table.
+    for  (var i = 0; i < prices.length; i++) {
       if (prices[i].value !== "") {
         totalprices = totalprices + Number(prices[i].value);
       }
     }
 
-    // That total will then be substracted from the pre-holiday budget. 
+    // That total will then be substracted from the pre-holiday budget.
     var remaining = budget - totalprices;
     document.getElementById("prebudget").innerHTML =
       "Here is your remaining budget of £" + remaining;
