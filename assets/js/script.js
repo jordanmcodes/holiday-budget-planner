ef7ce05c81d856cc1eea0c9b1c029070000e1828
Marn39 <210750650+Marn39@users.noreply.github.com>
2026-02-13 21:37:28 +0000

    crediting source material in the js file.

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ document.getElementById("buttonstart").addEventListener("click", startBudget);
 
 
 // If you press the button before entering in a number,
-// A message will appear instructing you to.
+// A message will appear instructing you to. Source W3 schools.
 function startBudget() {
 
   var budget = document.getElementById("holidaybudget").value;
@@ -17,14 +17,14 @@ function startBudget() {
      var prices = document.getElementsByClassName("Price");
      var totalprices = 0;
     // These functions will calculate the total
-    // for the expenses outlined in the table.
+    // for the expenses outlined in the table. Source W3 schools.
     for  (var i = 0; i < prices.length; i++) {
       if (prices[i].value !== "") {
         totalprices = totalprices + Number(prices[i].value);
       }
     }
 
-    // That total will then be substracted from the pre-holiday budget.
+    // That total will then be substracted from the pre-holiday budget. Source W3 schools.
     var remaining = budget - totalprices;
     document.getElementById("prebudget").innerHTML =
       "Here is your remaining budget of £" + remaining;
